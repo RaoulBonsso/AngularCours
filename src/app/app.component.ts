bb0aca396038ecbd99662c4765aff8ca478262a3
goldspide <raoulbonsso@gmail.com>
2025-01-15 15:52:41 +0100

    exercice 3 terminé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'AngularCours';
-  courseName = "Angular Avancé"
+  courseName = "Angular Avancé";
+  isDisabled = true;
 }
