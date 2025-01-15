cb00a7edf2d54f9a56c3e735f3a18ebb0edfd806
goldspide <raoulbonsso@gmail.com>
2025-01-15 16:28:21 +0100

    restructuration du code depuis et resolution des probleme present sur exercice 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoursesComponent } from "./courses/courses.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularCours';
-  courseName = "Angular Avancé";
-  isDisabled = true;
 
-  startCourse(){
-    console.log("Début du cours :", this.courseName);
-    this.isDisabled = false;
-  }
 }
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from'@angular/forms';  // Importation de FormsModule@Component({
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
+  courseName = "Angular Avancé";
+  isDisabled = true;
+  courseDescription = '';
+
+  startCourse(){
+    console.log("Début du cours :", this.courseName);
+    this.isDisabled = false;
+  }
 
 }
+
